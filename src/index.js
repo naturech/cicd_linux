f02259e5772ae9c6e0db0cbd2cc31bccb914beca
vheh23 <chajayeon7930@naver.com>
2024-12-10 23:50:14 +0900

    2nd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app= espress();
 const port=3000;
 app.get("/",(req,res) =>{
-    res.send("<h1> Incheon national university !! </h1>");
+    res.send("<h1> Incheon national university !!!! </h1>");
 });
 app.listen(port,()=>{
     console.log('Example app listening at http://localhost:${port}');
